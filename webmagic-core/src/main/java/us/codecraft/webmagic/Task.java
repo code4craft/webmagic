e1e25cb5e75f31f796c15037698ef34587d5e632
yihua.huang <code4crafter@gmail.com>
2013-06-20 08:21:48 +0800

    update javadoc

--- webmagic-core/src/main/java/us/codecraft/webmagic/Task.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/Task.java
@@ -1,12 +1,17 @@
 package us.codecraft.webmagic;
 
 /**
+ * 抓取任务的抽象接口。<br>
  * @author code4crafter@gmail.com <br>
  * Date: 13-6-18
  * Time: 下午2:57
  */
 public interface Task {
 
+    /**
+     * 返回唯一标志该任务的字符串，以区分不同任务。
+     * @return uuid
+     */
     public String getUUID();
 
 }
