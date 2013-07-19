5708b6e9c325e703d866fbbd68d9cb2e288bf364
yihua.huang <code4crafter@gmail.com>
2013-07-19 12:34:22 +0800

    +sina blog

--- webmagic-samples/src/main/java/us/codecraft/webmagic/samples/SinaBlogProcesser.java
+++ webmagic-samples/src/main/java/us/codecraft/webmagic/samples/SinaBlogProcesser.java
@@ -1,7 +1,8 @@
 package us.codecraft.webmagic.samples;
 
-import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 /**
@@ -15,7 +16,7 @@ public class SinaBlogProcesser implements PageProcessor {
 
     @Override
     public void process(Page page) {
-        page.addTargetRequests(page.getHtml().links().regex("(http://blog\\.sina\\.com\\.cn/s/blog_.*)").toStrings());
+        page.addTargetRequests(page.getHtml().xpath("//div[@class='articalfrontback SG_j_linedot1 clearfix']").links().toStrings());
         page.putField("title", page.getHtml().xpath("//div[@class='articalTitle']/h2"));
         page.putField("content",page.getHtml().xpath("//div[@id='articlebody']//div[@class='articalContent']"));
         page.putField("id",page.getUrl().regex("http://blog\\.sina\\.com\\.cn/s/blog_(\\w+)"));
@@ -26,9 +27,13 @@ public class SinaBlogProcesser implements PageProcessor {
     @Override
     public Site getSite() {
         if (site==null){
-            site = Site.me().setDomain("blog.sina.com.cn").addStartUrl("http://blog.sina.com.cn/flashsword20").setSleepTime(3000).
+            site = Site.me().setDomain("blog.sina.com.cn").addStartUrl("http://blog.sina.com.cn/s/blog_4701280b0102egl0.html").setSleepTime(3000).
                     setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
         }
         return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new SinaBlogProcesser()).run();
     }
 }
