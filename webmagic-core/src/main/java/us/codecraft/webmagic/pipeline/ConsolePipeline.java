54904851eafcee7e8211b4ff19e5c583f0ce9880
yihua.huang <code4crafter@gmail.com>
2013-07-26 21:22:57 +0800

    add list output support

--- webmagic-core/src/main/java/us/codecraft/webmagic/pipeline/ConsolePipeline.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/pipeline/ConsolePipeline.java
@@ -20,6 +20,15 @@ public class ConsolePipeline implements  Pipeline{
         }
         System.out.println("get page: "+resultItems.getRequest().getUrl());
         for (Map.Entry<String, Object> entry : resultItems.getAll().entrySet()) {
+            if (entry.getValue() instanceof Iterable) {
+                Iterable value = (Iterable) entry.getValue();
+                System.out.println(entry.getKey() + ":");
+                for (Object o : value) {
+                    System.out.println(o);
+                }
+            } else {
+                System.out.println(entry.getKey() + ":\t" + entry.getValue());
+            }
             System.out.println(entry.getKey()+":\t"+entry.getValue());
         }
     }
