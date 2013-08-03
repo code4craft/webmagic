145628557d39d162fc801c86f2f237a8d36f08f5
yihua.huang <code4crafter@gmail.com>
2013-08-03 18:01:17 +0800

    update afterextract api

--- webmagic-core/src/main/java/us/codecraft/webmagic/oo/ObjectPageProcessor.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/oo/ObjectPageProcessor.java
@@ -4,11 +4,13 @@ import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Selector;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -33,7 +35,7 @@ public class ObjectPageProcessor implements PageProcessor {
     }
 
 
-    public ObjectPageProcessor addPageModel(Class clazz){
+    public ObjectPageProcessor addPageModel(Class clazz) {
         PageModelExtractor pageModelExtractor = PageModelExtractor.create(clazz);
         targetUrlPatterns.addAll(pageModelExtractor.getTargetUrlPatterns());
         targetUrlPatterns.addAll(pageModelExtractor.getHelpUrlPatterns());
@@ -49,22 +51,34 @@ public class ObjectPageProcessor implements PageProcessor {
     public void process(Page page) {
         for (PageModelExtractor pageModelExtractor : pageModelExtractorList) {
             Object process = pageModelExtractor.process(page);
-            if (process==null){
+            if (process == null) {
                 page.getResultItems().setSkip(true);
             }
             postProcessPageModel(pageModelExtractor.getClazz(), process);
             page.putField(pageModelExtractor.getClazz().getCanonicalName(), process);
+            extractLinks(page, pageModelExtractor.getHelpUrlRegionSelector(), pageModelExtractor.getHelpUrlPatterns());
+            extractLinks(page, pageModelExtractor.getTargetUrlRegionSelector(), pageModelExtractor.getTargetUrlPatterns());
         }
-        for (String link : page.getHtml().links().all()) {
-            for (Pattern targetUrlPattern : targetUrlPatterns) {
-                if (targetUrlPattern.matcher(link).matches()){
-                    page.addTargetRequest(new Request(link));
+    }
+
+    private void extractLinks(Page page, Selector urlRegionSelector, List<Pattern> urlPatterns) {
+        List<String> links;
+        if (urlRegionSelector == null) {
+            links = page.getHtml().links().all();
+        } else {
+            links = urlRegionSelector.selectList(page.getHtml().toString());
+        }
+        for (String link : links) {
+            for (Pattern targetUrlPattern : urlPatterns) {
+                Matcher matcher = targetUrlPattern.matcher(link);
+                if (matcher.find()) {
+                    page.addTargetRequest(new Request(matcher.group(1)));
                 }
             }
         }
     }
 
-    protected void postProcessPageModel(Class clazz, Object object){
+    protected void postProcessPageModel(Class clazz, Object object) {
     }
 
     @Override
