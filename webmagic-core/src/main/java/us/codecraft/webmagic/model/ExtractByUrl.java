cf9cadf44210fb033d07ba04b37bfe6e3ffe5d81
yihua.huang <code4crafter@gmail.com>
2013-08-05 13:53:13 +0800

    add core

--- webmagic-core/src/main/java/us/codecraft/webmagic/model/ExtractByUrl.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/model/ExtractByUrl.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
+ * 定义类或者字段的抽取规则(从url中抽取，只支持正则表达式)。<br>
  * @author code4crafter@gmail.com <br>
  * @date: 13-8-1 <br>
  * Time: 下午8:40 <br>
