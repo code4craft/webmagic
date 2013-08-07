68fc832ce4395eb1dad0c115d841a32b48792edf
yihua.huang <code4crafter@gmail.com>
2013-08-07 23:36:48 +0800

    remove freemarker dp

--- webmagic-samples/src/test/java/us/codecraft/webmagic/processor/DiaoyuwengProcessorTest.java
+++ webmagic-samples/src/test/java/us/codecraft/webmagic/processor/DiaoyuwengProcessorTest.java
@@ -4,7 +4,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.pipeline.FilePipeline;
-import us.codecraft.webmagic.pipeline.FreemarkerPipeline;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.samples.DiaoyuwengProcessor;
 import us.codecraft.webmagic.scheduler.FileCacheQueueScheduler;
 
@@ -21,7 +21,7 @@ public class DiaoyuwengProcessorTest {
     @Test
     public void test() throws IOException {
         DiaoyuwengProcessor diaoyuwengProcessor = new DiaoyuwengProcessor();
-        FreemarkerPipeline pipeline = new FreemarkerPipeline("wordpress.ftl");
+        JsonFilePipeline pipeline = new JsonFilePipeline("/data/webmagic/");
         Spider.create(diaoyuwengProcessor).pipeline(new FilePipeline()).pipeline(pipeline).scheduler(new FileCacheQueueScheduler("/data/temp/webmagic/cache/")).
                 run();
     }
