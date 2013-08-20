eb99cf77557ed7385aa33aed6079fb5166af7bf9
yihua.huang <code4crafter@gmail.com>
2013-08-21 07:24:21 +0800

    update jar

---
+++ make.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mvn clean package
+cp ./webmagic-samples/target/webmagic-0.2.1.jar ./bin/
+rsync -avz --delete ./webmagic-samples/target/lib/ ./bin/lib/
+git add .
+git add -u
+git cim "update jar"
+git push
