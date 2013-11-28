486d9d276ff5b580af9788e7fb81e118821c4368
yihua.huang <code4crafter@gmail.com>
2013-11-28 18:23:51 +0800

    #45 Remove multi in ExtractBy

--- webmagic-extension/src/main/java/us/codecraft/webmagic/example/AppStore.java
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/example/AppStore.java
@@ -23,8 +23,11 @@ public class AppStore {
     @ExtractBy(type = ExtractBy.Type.JsonPath, value = "$..userRatingCount")
     private int userRatingCount;
 
-    @ExtractBy(type = ExtractBy.Type.JsonPath, value = "$..screenshotUrls",multi = true)
+    @ExtractBy(type = ExtractBy.Type.JsonPath, value = "$..screenshotUrls")
     private List<String> screenshotUrls;
+
+    @ExtractBy(type = ExtractBy.Type.JsonPath, value = "$..supportedDevices")
+    private List<String> supportedDevices;
 
     public static void main(String[] args) {
         AppStore appStore = OOSpider.create(Site.me(), AppStore.class).<AppStore>get("http://itunes.apple.com/lookup?id=653350791&country=cn&entity=software");
@@ -32,5 +35,6 @@ public class AppStore {
         System.out.println(appStore.description);
         System.out.println(appStore.userRatingCount);
         System.out.println(appStore.screenshotUrls);
+        System.out.println(appStore.supportedDevices);
     }
 }
--- webmagic-extension/src/main/java/us/codecraft/webmagic/model/annotation/ExtractByUrl.java
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/model/annotation/ExtractByUrl.java
@@ -33,6 +33,8 @@ public @interface ExtractByUrl {
      * Define whether the extractor return more than one result.
      * When set to 'true', the extractor return a list of string (so you should define the field as List). <br>
      *
+     * Deprecated since 0.4.2. This option is determined automatically by the class of field.
+     * @deprecated since 0.4.2
      * @return whether the extractor return more than one result
      */
     boolean multi() default false;
