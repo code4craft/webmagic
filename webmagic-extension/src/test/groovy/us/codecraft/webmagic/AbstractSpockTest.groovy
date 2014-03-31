6cabd047009f80e6dc690672fdfc26c7edbc76a0
Linker Lin <linker.lin@live.com>
2014-03-31 22:00:47 +0800

    增加groovy支持

---
+++ webmagic-extension/src/test/groovy/us/codecraft/webmagic/AbstractSpockTest.groovy
@@ -0,0 +1,9 @@
+package us.codecraft.webmagic;
+
+import spock.lang.Specification;
+
+/**
+ * Created by linkerlin on 3/31/14.
+ */
+public class AbstractSpockTest extends Specification {
+}
---
+++ webmagic-extension/src/test/groovy/us/codecraft/webmagic/utils/UtilTest.groovy
@@ -0,0 +1,14 @@
+package us.codecraft.webmagic.utils
+
+import us.codecraft.webmagic.AbstractSpockTest
+/**
+ * Created by linkerlin on 3/31/14.
+ */
+class UtilTest extends AbstractSpockTest{
+
+    def "You shall pass!"(){
+        expect:
+            1==1
+    }
+
+}
