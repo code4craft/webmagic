c143fc662cb0e21ced7ed084aff63d25b09b5b3b
yihua.huang <code4crafter@gmail.com>
2014-04-05 18:17:48 +0800

    add SubPageProcessor #86

---
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/handler/SubPageProcessor.java
@@ -0,0 +1,33 @@
+package us.codecraft.webmagic.handler;
+
+import us.codecraft.webmagic.Page;
+
+/**
+ * @author code4crafter@gmail.com
+ * @date 14-4-5
+ */
+public interface SubPageProcessor {
+
+    /**
+     * Check whether the SubPageProcessor can process the page.<br></br>
+     * Please DO NOT change page status in this method.
+     *
+     * @param page
+     * @return
+     */
+    public boolean match(Page page);
+
+    /**
+     *
+     * process the page, extract urls to fetch, extract the data and store
+     *
+     * @param page
+     * @return whether continue to match
+     */
+    public MatchOtherProcessor process(Page page);
+
+    public enum MatchOtherProcessor {
+        YES, NO;
+    }
+
+}
