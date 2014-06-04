4efd471840c5e8c14c36a3d7b65023b7b08caf1b
yihua.huang <code4crafter@gmail.com>
2014-06-04 22:46:03 +0800

    remove duplicate jar

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn dependency:copy-dependencies -DoutputDirectory=target/lib
+mvn clean dependency:copy-dependencies -DoutputDirectory=target/lib
 rsync -avz --delete ./webmagic-samples/target/lib/ ./lib/
