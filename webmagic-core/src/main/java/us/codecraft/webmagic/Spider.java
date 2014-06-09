2f89cfc31a31887fdd6f634d9e708cd9031bfb36
zwf <yxssfxwzy@sina.com>
2014-06-09 13:32:02 +0800

    add test and fix bug of proxy module

--- webmagic-core/src/main/java/us/codecraft/webmagic/Spider.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/Spider.java
@@ -424,6 +424,8 @@ public class Spider implements Runnable, Task {
                 pipeline.process(page.getResultItems(), this);
             }
         }
+        //for proxy status management
+        request.putExtra(Request.STATUS_CODE, page.getStatusCode());
         sleep(site.getSleepTime());
     }
 
