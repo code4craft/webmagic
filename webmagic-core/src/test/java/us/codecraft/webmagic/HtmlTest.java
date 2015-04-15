67eb632f4dc19480400893f22b1d7ad2301edfae
yihua.huang <code4crafter@gmail.com>
2015-04-15 08:31:45 +0800

    test for issue #200

--- webmagic-core/src/test/java/us/codecraft/webmagic/HtmlTest.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/HtmlTest.java
@@ -2,6 +2,7 @@ package us.codecraft.webmagic;
 
 import org.junit.Test;
 import us.codecraft.webmagic.selector.Html;
+import us.codecraft.webmagic.selector.Selectable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,5 +31,19 @@ public class HtmlTest {
 		Html.DISABLE_HTML_ENTITY_ESCAPE = false;
 		Html html = new Html("aaaaaaa&b");
 		assertThat(html.regex("(aaaaaaa&amp;b)").toString()).isEqualTo("aaaaaaa&amp;b");
+	}
+
+	@Test
+	public void testAHrefExtract(){
+		Html html = new Html("<a data-tip=\"p$t$xxx\" href=\"/xx/xx\">xx</a>");
+		assertThat(html.links().all()).contains("/xx/xx");
+	}
+
+	@Test
+	public void testNthNodesGet(){
+		Html html = new Html("<a data-tip=\"p$t$xxx\" href=\"/xx/xx\">xx</a>");
+		assertThat(html.xpath("//a[1]/@href").get()).isEqualTo("/xx/xx");
+		Selectable selectable = html.xpath("//a[1]").nodes().get(0);
+		assertThat(selectable.xpath("@href").get()).isEqualTo("/xx/xx");
 	}
 }
