c6ae2e470e9a9cfb2bc46badb7671707ca499bf8
qiwang@dorontech.com <qiwang@dorontech.com>
2016-06-15 09:59:41 +0800

    Enhancement: Add Exception for Spider.Run onError, in case SpiderListener wants exception detail

--- webmagic-extension/src/main/java/us/codecraft/webmagic/monitor/SpiderMonitor.java
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/monitor/SpiderMonitor.java
@@ -89,6 +89,12 @@ public class SpiderMonitor {
             errorCount.incrementAndGet();
         }
 
+        @Override
+        public void onError(Request request, Exception e) {
+            errorUrls.add(request.getUrl());
+            errorCount.incrementAndGet();
+        }
+
         public AtomicInteger getSuccessCount() {
             return successCount;
         }
