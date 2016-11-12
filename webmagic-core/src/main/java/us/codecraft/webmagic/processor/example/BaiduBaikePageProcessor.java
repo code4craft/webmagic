fef88cc5001c385852acf6878c2569bd37e9e4ec
karlind <miky@karls.top>
2016-11-12 22:15:53 +0800

    Update examples

--- webmagic-core/src/main/java/us/codecraft/webmagic/processor/example/BaiduBaikePageProcessor.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/processor/example/BaiduBaikePageProcessor.java
@@ -16,12 +16,14 @@ import java.util.List;
 public class BaiduBaikePageProcessor implements PageProcessor {
 
     private Site site = Site.me()//.setHttpProxy(new HttpHost("127.0.0.1",8888))
-            .setRetryTimes(3).setSleepTime(1000).setUseGzip(true);
+                            .setRetryTimes(3)
+                            .setSleepTime(1000)
+                            .setUseGzip(true);
 
     @Override
     public void process(Page page) {
-        page.putField("name", page.getHtml().css("h1.title div.lemmaTitleH1","text").toString());
-        page.putField("description", page.getHtml().xpath("//div[@id='lemmaContent-0']//div[@class='para']/allText()"));
+        page.putField("name", page.getHtml().css("h1","text").toString());
+        page.putField("description", page.getHtml().xpath("//div[@class='para']/allText()"));
     }
 
     @Override
@@ -33,7 +35,7 @@ public class BaiduBaikePageProcessor implements PageProcessor {
         //single download
         Spider spider = Spider.create(new BaiduBaikePageProcessor()).thread(2);
         String urlTemplate = "http://baike.baidu.com/search/word?word=%s&pic=1&sug=1&enc=utf8";
-        ResultItems resultItems = spider.<ResultItems>get(String.format(urlTemplate, "水力发电"));
+        ResultItems resultItems = spider.get(String.format(urlTemplate, "水力发电"));
         System.out.println(resultItems);
 
         //multidownload
@@ -42,7 +44,7 @@ public class BaiduBaikePageProcessor implements PageProcessor {
         list.add(String.format(urlTemplate,"太阳能"));
         list.add(String.format(urlTemplate,"地热发电"));
         list.add(String.format(urlTemplate,"地热发电"));
-        List<ResultItems> resultItemses = spider.<ResultItems>getAll(list);
+        List<ResultItems> resultItemses = spider.getAll(list);
         for (ResultItems resultItemse : resultItemses) {
             System.out.println(resultItemse.getAll());
         }
