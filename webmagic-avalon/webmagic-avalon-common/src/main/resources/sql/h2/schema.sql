50494efb25d4367fc476579168396196e6694f8f
karlind <miky@karls.top>
2016-11-13 19:20:32 +0800

    Remove script and avalon modules

--- webmagic-avalon/webmagic-avalon-common/src/main/resources/sql/h2/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE DynamicClass(
-  Id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `ClassName` varchar(200) NOT NULL,
-  `SourceCode` text NOT NULL,
-  `AddTime` datetime NOT NULL,
-  `UpdateTime` datetime NOT NULL,
-  UNIQUE INDEX `un_class_name` (`ClassName`)
-);
