fc702fd3b659f156a738057b5066c59e0d542dc2
yihua.huang <code4crafter@gmail.com>
2017-03-11 11:31:15 +0800

    introduce mockito for test

---
+++ webmagic-samples/src/test/java/us/codecraft/webmagic/samples/scheduler/DuplicateRemovedSchedulerTest.java
@@ -0,0 +1,17 @@
+package us.codecraft.webmagic.samples.scheduler;
+
+import org.junit.Test;
+
+/**
+ * @author code4crafter@gmail.com
+ *         Date: 17/3/11
+ *         Time: 上午11:26
+ */
+public class DuplicateRemovedSchedulerTest {
+
+    @Test
+    public void testDuplicateRemoved() throws Exception {
+        
+
+    }
+}
