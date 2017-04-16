eb632a93d36585be2d159e32ff17b26340e72762
yihua.huang <code4crafter@gmail.com>
2017-04-16 10:43:00 +0800

    SimpleProxyProvider #535

--- webmagic-core/src/main/java/us/codecraft/webmagic/proxy/Proxy.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/proxy/Proxy.java
@@ -38,4 +38,36 @@ public class Proxy {
 	public String getPassword() {
 		return password;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Proxy proxy = (Proxy) o;
+
+		if (port != proxy.port) return false;
+		if (host != null ? !host.equals(proxy.host) : proxy.host != null) return false;
+		if (username != null ? !username.equals(proxy.username) : proxy.username != null) return false;
+		return password != null ? password.equals(proxy.password) : proxy.password == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = host != null ? host.hashCode() : 0;
+		result = 31 * result + port;
+		result = 31 * result + (username != null ? username.hashCode() : 0);
+		result = 31 * result + (password != null ? password.hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Proxy{" +
+				"host='" + host + '\'' +
+				", port=" + port +
+				", username='" + username + '\'' +
+				", password='" + password + '\'' +
+				'}';
+	}
 }
