bb0eb69acff4bf8f2e50119ea35c4318182c17b9
yihua.huang <code4crafter@gmail.com>
2017-06-03 10:42:17 +0800

    update ZhihuPageProcessor example

--- webmagic-core/src/main/java/us/codecraft/webmagic/processor/example/ZhihuPageProcessor.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/processor/example/ZhihuPageProcessor.java
@@ -16,9 +16,9 @@ public class ZhihuPageProcessor implements PageProcessor {
     @Override
     public void process(Page page) {
         page.addTargetRequests(page.getHtml().links().regex("https://www\\.zhihu\\.com/question/\\d+/answer/\\d+.*").all());
-        page.putField("title", page.getHtml().xpath("//h2[@class='zm-item-title']/a/text()").toString());
-        page.putField("question", page.getHtml().xpath("//div[@id='zh-question-detail']//tidyText()").toString());
-        page.putField("answer", page.getHtml().xpath("//div[@id='zh-question-answer-wrap']//div[@class='zm-editable-content']/tidyText()").toString());
+        page.putField("title", page.getHtml().xpath("//h1[@class='QuestionHeader-title']/text()").toString());
+        page.putField("question", page.getHtml().xpath("//div[@class='QuestionRichText']//tidyText()").toString());
+        page.putField("answer", page.getHtml().xpath("//div[@class='QuestionAnswer-content']/tidyText()").toString());
         if (page.getResultItems().get("title")==null){
             //skip this page
             page.setSkip(true);
