adf545483797392333135a3d7900b31b71110d9d
yihy <2100087178@qq.com>
2017-11-30 11:35:12 +0800

    [Fix] 修正错误方法名

--- webmagic-extension/src/main/java/us/codecraft/webmagic/scheduler/RedisScheduler.java
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/scheduler/RedisScheduler.java
@@ -61,7 +61,7 @@ public class RedisScheduler extends DuplicateRemovedScheduler implements Monitor
         Jedis jedis = pool.getResource();
         try {
             jedis.rpush(getQueueKey(task), request.getUrl());
-            if (CheckForAdditionalInfo(request)) {
+            if (checkForAdditionalInfo(request)) {
                 String field = DigestUtils.shaHex(request.getUrl());
                 String value = JSON.toJSONString(request);
                 jedis.hset((ITEM_PREFIX + task.getUUID()), field, value);
@@ -71,7 +71,7 @@ public class RedisScheduler extends DuplicateRemovedScheduler implements Monitor
         }
     }
 
-    private boolean CheckForAdditionalInfo(Request request) {
+    private boolean checkForAdditionalInfo(Request request) {
         if (request == null) {
             return false;
         }
