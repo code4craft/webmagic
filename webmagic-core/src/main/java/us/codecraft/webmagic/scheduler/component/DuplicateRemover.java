203d8239e90a05e1dafcf5cf266584a787bd2913
Jean-Debout Gatari <jeandebout.gatari.etu@univ-lille.fr>
2021-04-04 16:33:44 +0200

    Add up and completion of javadoc

--- webmagic-core/src/main/java/us/codecraft/webmagic/scheduler/component/DuplicateRemover.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/scheduler/component/DuplicateRemover.java
@@ -4,7 +4,7 @@ import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Task;
 
 /**
- * Remove duplicate requests.
+ * Remove duplicate requests.</br>
  * @author code4crafer@gmail.com
  * @since 0.5.1
  */
