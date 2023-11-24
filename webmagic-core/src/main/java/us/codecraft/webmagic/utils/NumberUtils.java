622ed5a17f98ee1625222452096741ebe16dfe85
Harikrishna <harikrishna553@gmail.com>
2023-11-24 10:07:04 +0530

    Refactor compareLong method using Long.compare, corrected the local variable name (#1136)

--- webmagic-core/src/main/java/us/codecraft/webmagic/utils/NumberUtils.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/utils/NumberUtils.java
@@ -6,12 +6,6 @@ package us.codecraft.webmagic.utils;
 public abstract class NumberUtils {
 
     public static int compareLong(long o1, long o2) {
-        if (o1 < o2) {
-            return -1;
-        } else if (o1 == o2) {
-            return 0;
-        } else {
-            return 1;
-        }
+    	return Long.compare(o1, o2);
     }
 }
