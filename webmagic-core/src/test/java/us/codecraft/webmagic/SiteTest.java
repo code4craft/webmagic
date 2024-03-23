80842d72db35b22d93e34d7773251c9bec9a9de9
ayushi250317 <157420261+ayushi250317@users.noreply.github.com>
2024-03-23 02:22:30 -0300

    Added test cases for improving line and branch coverage (#1150)
    * Initial Commit
    
    * Assignment 1 Submission

--- webmagic-core/src/test/java/us/codecraft/webmagic/SiteTest.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/SiteTest.java
@@ -1,8 +1,12 @@
 package us.codecraft.webmagic;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -14,4 +18,23 @@ public class SiteTest {
         assertEquals(StandardCharsets.UTF_8.name(), site.getDefaultCharset());
     }
 
+    @Test
+    public void addCookieTest(){
+        Site site=Site.me().setDefaultCharset(StandardCharsets.UTF_8.name());
+        site.addCookie("cookieDefault","cookie-webmagicDefault");
+        String firstDomain="example.com";
+        String secondDomain="exampleCopy.com";
+        site.addCookie(firstDomain, "cookie", "cookie-webmagic");
+        site.addCookie(firstDomain, "cookieCopy", "cookie-webmagicCopy");
+        site.addCookie(secondDomain, "cookie", "cookie-webmagic");
+        Map<String, Map<String, String>> allCookies = site.getAllCookies();
+        List<String> domains=new ArrayList<>();
+        for(String key : allCookies.keySet()){
+            domains.add(key);
+        }
+        assertEquals("cookie-webmagic", allCookies.get(firstDomain).get("cookie"));
+        assertEquals("cookie-webmagicCopy", allCookies.get(firstDomain).get("cookieCopy"));
+        assertEquals("cookie-webmagic", allCookies.get(secondDomain).get("cookie"));
+        assertEquals(2, domains.size());
+    }
 }
